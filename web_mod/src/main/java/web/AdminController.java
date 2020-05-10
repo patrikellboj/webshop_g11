3f32f9c6899cc4840ff57cf6bf9c0811037685bc
MarioSamperi75 <mariosss@libero.it>
2020-05-10 11:03:14 +0200

    flera user uppgifter i adminsidan

--- web_mod/src/main/java/web/AdminController.java
+++ web_mod/src/main/java/web/AdminController.java
@@ -16,11 +16,19 @@ public class AdminController implements Serializable {
     @EJB
     AdminHandlerLocal adminHandlerLocal;
     private List <Product> products = new ArrayList<>();
-    private String selectedUserUsername;
-    private String selectedUserPassword;
-    private Enum selectedUserRole;
     private boolean renderOrders = false;
     private int orderCounter = 0;
+
+    private String selectedFirstName;
+    private String selectedLastName;
+    private String selectedAddress;
+    private String selectedUserUsername;
+    private String selectedUserPassword;
+    private String selectedEmail;
+    private Enum selectedUserRole;
+
+
+
 
     public List <User> getUsers() {
         return adminHandlerLocal.getUsersFromDB();
@@ -33,9 +41,14 @@ public class AdminController implements Serializable {
     public void getUserInfo(String username) {
         for (User user : getUsers()) {
             if (user.getUsername().equals(username)) {
+                selectedFirstName = user.getFirstName();
+                selectedLastName = user.getLastName();
+                selectedAddress = user.getAddress();
                 selectedUserUsername = user.getUsername();
                 selectedUserPassword = user.getPassword();
+                selectedEmail = user.getEmail();
                 selectedUserRole = user.getRole();
+
                 renderOrders = true;
             }
         }
@@ -66,6 +79,22 @@ public class AdminController implements Serializable {
 
     public List<Product> getProducts() {
         return products;
+    }
+
+    public String getSelectedFirstName() {
+        return selectedFirstName;
+    }
+
+    public String getSelectedLastName() {
+        return selectedLastName;
+    }
+
+    public String getSelectedAddress() {
+        return selectedAddress;
+    }
+
+    public String getSelectedEmail() {
+        return selectedEmail;
     }
 
     public void setProducts(List<Product> products) {
